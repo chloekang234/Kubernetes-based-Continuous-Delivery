9e565da6deef2ca38e5cbf2e2d5e1ad46c4feaf6
chloekang234 <yk234@duke.edu>
2023-02-20 19:57:19 -0500

    initial commit

---
+++ amazon-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEM convention
+    <div className="app">
+      <h1>Hello, let's build the Amazon Store🚀</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAMiUwVq2R-um5jx6U5W7gNY7u0BykB4JQ",
+    authDomain: "app-746eb.firebaseapp.com",
+    projectId: "app-746eb",
+    storageBucket: "app-746eb.appspot.com",
+    messagingSenderId: "616953328806",
+    appId: "1:616953328806:web:5619c4a538b86982ceb067",
+    measurementId: "G-19TXNEPTL4"
+  };
