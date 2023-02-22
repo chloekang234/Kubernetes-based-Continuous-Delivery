cba3a73734bf2be16aa830a1d4c819397f02df31
cholekang234 <yunseon.kang@duke.edu>
2023-02-21 20:52:49 -0500

    update

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from "react-router-dom"
 
 function Header() {
   return (
     <div className='header'>
-        <img 
-            className="header__logo"
-            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        />
+        <Link to="/">
+            <img 
+                className="header__logo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            />
+        </Link>
+    
         <div 
         className="header__search">
             <input
@@ -37,11 +41,13 @@ function Header() {
                 <span className='header__optionLineTwo'>Prime</span>
             </div>
 
-
-            <div className='header__optionBasket'>
-                <ShoppingBasketIcon />
-                <span className='header__optionLineTwo header__basketCount'>0</span>
-            </div>
+            <Link to='/checkout'>
+                <div className='header__optionBasket'>
+                    <ShoppingBasketIcon />
+                    <span className='header__optionLineTwo header__basketCount'>0</span>
+                </div>
+            </Link>
+            
         </div>
     </div>
   )
